76ef091d17a0b4b3386b2cc02ca277ed24ceb5da
Sean Breckenridge <seanbrecke@gmail.com>
2020-08-22 00:09:02 -0700

    link to documentaton in help message

--- no-db-static-shorturl.go
+++ no-db-static-shorturl.go
@@ -146,6 +146,8 @@ curl --header "Content-Type: application/json" --request POST --data '{"key":"yo
 or to specify the path to create the shorturl on:
 
 curl --header "Content-Type: application/json" --request POST --data '{"key":"your_secret_key","url":"https://sean.fish","hash":"short"}' http://localhost:8040
+
+For more info see https://gitlab.com/seanbreckenridge/no-db-static-shorturl
 `)
 				return
 			}
